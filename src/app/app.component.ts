e9744522c114bb0dcc6b0314442c644c7ea584ae
harrari <omar.harrari@gmail.com>
2017-11-10 16:48:19 +0000

    menu navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,23 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import {GalleryPage} from "../pages/gallery/gallery";
+import {PlacesPage} from "../pages/places/places";
+import {MeteoPage} from "../pages/meteo/meteo";
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
+  menus = [
+    {title:"Gallery" , component:GalleryPage },
+    {title:"Places" , component:PlacesPage },
+    {title:"Meteo" , component:MeteoPage }
+  ]
+  onPage(p){
+    this.rootPage = p.component;
+  }
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
